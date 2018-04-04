1bc4d22f8061a71e4bf124f890649471764e9e31
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-04-04 01:46:27 -0300

    Bootstrap adicionado

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,16 +2,17 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:dart_fullstack/src/home/home_component.dart';
 import 'package:dart_fullstack/src/student/student_component.dart';
+import 'package:ng_bootstrap/ng_bootstrap.dart';
 
 @Component(
   selector: 'app-component',
   templateUrl: 'app_component.html',
-  directives: const [ROUTER_DIRECTIVES],
+  directives: const [ROUTER_DIRECTIVES, BS_DIRECTIVES],
 )
 @RouteConfig(
   const [
     const Route(
-      path: '/home',
+      path: '/',
       name: 'Home',
       component: HomeComponent,
       useAsDefault: true
